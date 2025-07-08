7215672e0bb9b61e6cd99ef0db875c0caf5cf54b
VickyRemy <vicky240594@gmail.com>
2025-07-08 21:05:10 +0200

    Added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+function updateWeather(response){
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = response.data.temperature.current;
+}
+function searchCity(city) {
+    let apiKey ="09bdf26t323b4a9f4410fdob3dbedc7c";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeather);
+
+}
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
-
-
-
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
