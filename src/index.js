c83007b9d43d8aaba0d115032265ac14bf41f992
VickyRemy <vicky240594@gmail.com>
2025-07-08 23:17:11 +0200

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 function updateWeather(response){
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = response.data.temperature.current;
+    let temperature = response.data.temperature.current;
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date (response.data.time * 1000);
+    console.log(response.data.condition.description)
+
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/hr`;
+    timeElement.innerHTML = formatDate(date);
+    
+    temperatureElement.innerHTML = Math.round(temperature)
+}
+function formatDate(date) {
+    let hours= date.getHours();
+    let minutes= date.getMinutes();
+    let days = [
+                "Sunday",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+            ];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`
 }
 function searchCity(city) {
     let apiKey ="09bdf26t323b4a9f4410fdob3dbedc7c";
